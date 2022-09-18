de3d8270189c3f2b834f3a862f1db52b04e159a5
Amy Gardiner <amymgardiner@gmail.com>
2022-09-17 22:01:42 -0500

    added some comments to the code

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -22,11 +22,12 @@ const commentController = {
       .catch((err) => res.json(err));
   },
 
+  // add reply to comment
   addReply({ params, body }, res) {
     Comment.findOneAndUpdate(
       { _id: params.commentId },
       { $push: { replies: body } },
-      { new: true }
+      { new: true, runValidators: true }
     )
       .then((dbPizzaData) => {
         if (!dbPizzaData) {
@@ -35,17 +36,6 @@ const commentController = {
         }
         res.json(dbPizzaData);
       })
-      .catch((err) => res.json(err));
-  },
-
-  // remove reply
-  removeReply({ params }, res) {
-    Comment.findOneAndUpdate(
-      { _id: params.commentId },
-      { $pull: { replies: { replyId: params.replyId } } },
-      { new: true }
-    )
-      .then((dbPizzaData) => res.json(dbPizzaData))
       .catch((err) => res.json(err));
   },
 
@@ -69,6 +59,17 @@ const commentController = {
         }
         res.json(dbPizzaData);
       })
+      .catch((err) => res.json(err));
+  },
+
+  // remove reply
+  removeReply({ params }, res) {
+    Comment.findOneAndUpdate(
+      { _id: params.commentId },
+      { $pull: { replies: { replyId: params.replyId } } },
+      { new: true }
+    )
+      .then((dbPizzaData) => res.json(dbPizzaData))
       .catch((err) => res.json(err));
   },
 };
