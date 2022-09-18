384c6bd76c82a62ef663d029dbe0744381589461
Amy Gardiner <amymgardiner@gmail.com>
2022-09-17 23:16:13 -0500

    updated code compared to final lesson code

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -3,7 +3,7 @@ const { Comment, Pizza } = require('../models');
 const commentController = {
   // add comment to pizza
   addComment({ params, body }, res) {
-    console.log(body);
+    console.log(params);
     Comment.create(body)
       .then(({ _id }) => {
         return Pizza.findOneAndUpdate(
@@ -13,6 +13,7 @@ const commentController = {
         );
       })
       .then((dbPizzaData) => {
+        console.log(dbPizzaData);
         if (!dbPizzaData) {
           res.status(404).json({ message: 'No pizza found with this id!' });
           return;
@@ -61,7 +62,6 @@ const commentController = {
       })
       .catch((err) => res.json(err));
   },
-
   // remove reply
   removeReply({ params }, res) {
     Comment.findOneAndUpdate(
